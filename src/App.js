c5dc3ed6889f7190afdf9783a9d88c7f4ebce4e3
x <ackelboffin96@gmail.com>
2025-05-17 13:58:05 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "./styles.css";
+import { Navbar } from "./components/Navbar";
+
+export default function App() {
+  return (
+    <>
+      <Navbar />
+    </>
+  );
+}
---
+++ src/components/Navbar.js
@@ -0,0 +1,7 @@
+export function Navbar() {
+  return (
+    <>
+      <nav className="">Hello World</nav>
+    </>
+  );
+}
